1ca8ccfa07b653c97661f5833b88f65f7aa58094
AVVARI BALA KRISHNA <23MIC7254@VITAPUV.EDU>
2026-09-15 17:21:38 +0530

    line in main branch

--- demo/src/main/java/FirstLab/Demo/log/DemoApplication.java
+++ demo/src/main/java/FirstLab/Demo/log/DemoApplication.java
@@ -8,6 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		
+		//eddited the same line in main branch
 	}
 }
